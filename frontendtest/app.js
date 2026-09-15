a8c41c574dafb970d30768fdfaa0fe1c59cdcbcc
tlebeitsuk <tobias@iterlabs.fi>
2026-09-15 12:57:21 +0300

    list, delete, update students

--- frontendtest/app.js
+++ frontendtest/app.js
@@ -1,9 +1,49 @@
+const backend = "http://localhost:3000"
 
 async function getStudents() {
-  const response = await fetch("http://localhost:3000/students")
+  const response = await fetch(`${backend}/students`)
   const students = await response.json()
 
-  console.log(students)
+  const list = document.querySelector("#students")
+
+  students.forEach(student => {
+    const li = document.createElement("li")
+    const deleteButton = document.createElement("button")
+    const editButton = document.createElement("button")
+
+    li.textContent = student.name
+    deleteButton.textContent = "Delete"
+    editButton.textContent = "Edit"
+
+    deleteButton.addEventListener("click", async () => {
+      await fetch(`${backend}/students/${student.id}`, {
+        method: "DELETE"
+      })
+      
+     li.remove()
+    })
+
+    editButton.addEventListener("click", async () => {
+      const newName = prompt("New name:")
+      
+      await fetch(`${backend}/students/${student.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+           name: newName
+        })
+      })
+
+      li.firstChild.textContent = newName
+    })
+
+    li.append(editButton)
+    li.append(deleteButton)
+    list.append(li)
+  })
 }
 
 getStudents()
+
